532b2ff1e0f92243a62fa231ec7c2e746950b20b
gafriel <gafrieljunior@gmail.com>
2019-07-14 23:00:03 +0800

    #3 crud blog done

--- routes/api.php
+++ routes/api.php
@@ -32,3 +32,11 @@ Route::group(['prefix' => 'religion'], function () {
     Route::put('update', 'Api\ReligionController@update');
     Route::delete('delete', 'Api\ReligionController@delete');
 });
+
+Route::group(['prefix' => 'blog'], function () {
+    Route::post('create', 'Api\BlogController@create');
+    Route::get('list', 'Api\BlogController@list');
+    Route::get('read', 'Api\BlogController@read');
+    Route::put('update', 'Api\BlogController@update');
+    Route::delete('delete', 'Api\BlogController@delete');
+});
