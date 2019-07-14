9f3b8e608f4c5c354742b10f32e3367064102651
Fachreza Muslim <fachreza.muslim7396@gmail.com>
2019-07-14 13:41:10 +0800

    #4 CRUD Religion done

--- app/Religion.php
+++ app/Religion.php
@@ -7,4 +7,16 @@ use Illuminate\Database\Eloquent\Model;
 class Religion extends Model
 {
     protected $guarded = [];
+
+    static function getData($request) {
+        $query = "SELECT * FROM religions";
+
+        return \DB::select($query);
+    }
+
+    static function readData($id) {
+        $query = "SELECT * FROM religions AS r WHERE r.id = $id";
+
+        return \DB::select($query)[0];
+    }
 }
