1f660eebf4f05af38e147f813483f3b33b678358
dimasdwipa <dimasdwiparaga07@gmail.com>
2019-07-21 12:57:24 +0800

    #26 Students CRUD

--- routes/api.php
+++ routes/api.php
@@ -13,7 +13,7 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::get('/', function() {
+Route::get('/', function () {
     return response()->json("Sekolahku API", 200);
 });
 
@@ -31,4 +31,12 @@ Route::group(['prefix' => 'religion'], function () {
     Route::get('read', 'Api\ReligionController@read');
     Route::put('update', 'Api\ReligionController@update');
     Route::delete('delete', 'Api\ReligionController@delete');
+});
+
+Route::group(['prefix' => 'student'], function () {
+    Route::post('create', 'Api\StudentController@create');
+    Route::get('list', 'Api\StudentController@list');
+    Route::get('read', 'Api\StudentController@read');
+    Route::put('update', 'Api\StudentController@update');
+    Route::delete('delete', 'Api\StudentController@delete');
 });
